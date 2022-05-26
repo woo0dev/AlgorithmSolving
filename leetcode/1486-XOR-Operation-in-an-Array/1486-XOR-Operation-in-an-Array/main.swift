a5f72825b6e627d50e8771ba829d3cef439467b9
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-05-27 00:04:14 +0900

    1486-XOR-Operation-in-an-Array 수정

--- leetcode/1486-XOR-Operation-in-an-Array/1486-XOR-Operation-in-an-Array/main.swift
+++ leetcode/1486-XOR-Operation-in-an-Array/1486-XOR-Operation-in-an-Array/main.swift
@@ -2,12 +2,16 @@ func xorOperation(_ n: Int, _ start: Int) -> Int {
     var result = 0
     var arr: [Int] = []
     
-    for i in start..<n {
-        arr.append(2*i)
+    for i in start..<start+n {
+        arr.append(start+2*arr.count)
     }
-    result = arr[0]^arr[1]
-    for i in 1..<arr.count-1 {
-        result = result^arr[i+1]
+    if arr.count > 1 {
+        result = arr[0]^arr[1]
+        for i in 1..<arr.count-1 {
+            result = result^arr[i+1]
+        }
+    } else {
+        result = arr[0]
     }
     
     return result
