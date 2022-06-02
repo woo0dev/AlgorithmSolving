8279ab905e2d6d3ac371e390672cff1525470b96
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-06-02 23:59:20 +0900

    clear

---
+++ leetcode/1588-Sum-of-All-Odd-Length-Subarrays/1588-Sum-of-All-Odd-Length-Subarrays/main.swift
@@ -0,0 +1,19 @@
+func sumOddLengthSubarrays(_ arr: [Int]) -> Int {
+    var result = 0
+    var temp = 2
+    for i in 0..<arr.count {
+        result += arr[i]
+        while i+temp < arr.count {
+            for j in i..<i+temp+1 {
+                result += arr[j]
+                print(result)
+            }
+            temp += 2
+        }
+        temp = 2
+    }
+    
+    return result
+}
+
+print(sumOddLengthSubarrays([1,4,2,5,3]))
