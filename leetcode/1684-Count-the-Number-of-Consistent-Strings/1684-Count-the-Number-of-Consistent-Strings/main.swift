8fdaead2b04d8d0038c8aa7ecd14974f177bc933
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-07-16 19:07:00 +0900

    1684-Count-the-Number-of-Consistent-Strings solving

---
+++ leetcode/1684-Count-the-Number-of-Consistent-Strings/1684-Count-the-Number-of-Consistent-Strings/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  1684-Count-the-Number-of-Consistent-Strings
+//
+//  Created by woo0 on 2022/07/16.
+//
+
+import Foundation
+
+func countConsistentStrings(_ allowed: String, _ words: [String]) -> Int {
+	var result = 0
+	let charArr = allowed.map { return $0 }
+	let arr = 
+	
+	
+	return result
+}
+
+print(countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]))
